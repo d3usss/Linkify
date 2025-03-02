43815bb85468056dc35772b579326c7c7c6a059b
Łukasz Migut <real.d3us@gmail.com>
2025-03-02 18:25:04 +0100

    Add database and database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [UrlModule],
+  imports: [
+    UrlModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'dev',
+      password: 'dev',
+      database: 'linkify',
+      entities: [],
+      synchronize: true,
+      dropSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
